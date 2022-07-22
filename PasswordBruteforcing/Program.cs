c9f131f639986e47bda1d8b1109eb47f02868998
Alyoshkin Dmitry <alyoshkin.dmitry@yandex.ru>
2022-07-22 15:53:58 +0300

    First try coding

--- PasswordBruteforcing/Program.cs
+++ PasswordBruteforcing/Program.cs
@@ -10,9 +10,52 @@ namespace PasswordBruteforcing
     {
         public static void Main()
         {
-            
+            string passwordExample = null;
+            string passwordSighns = null;
+            while (true)
+            {
+                Console.WriteLine("Введите примерный пароль, заполняя звездочками");
+                Console.WriteLine("неизвестные места, как в примере ниже:");
+                Console.WriteLine("123****89");
+                passwordExample = Console.ReadLine();
+                Console.WriteLine("Теперь введите набор предполагаемых подряд");
+                passwordSighns = Console.ReadLine();
+                Console.WriteLine("Предполагаемый список паролей:");
+                
+                WritePasswordList(passwordExample, passwordSighns);
+
+                Console.WriteLine();
+            }
         }
 
+        public static void WritePasswordList(string passwordExample, string passwordSighns)
+        {
+            int position = 0;
+            WritePasswordList(passwordExample, passwordSighns, position);
+        }
 
+        public static void WritePasswordList(string passwordExample, string passwordSighns, int position)
+        {
+            var password = passwordExample.ToArray();
+
+            if (position == password.Length)
+                Console.WriteLine(MakeString(password));
+
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (passwordExample[position] == '*')
+                    password[position] = passwordSighns[i];
+                WritePasswordList(MakeString(password), passwordSighns, position+1);
+            }
+
+        }
+
+        public static string MakeString(char[] passwordSighns)
+        {
+            var stringToReturn = new StringBuilder();
+            foreach (var sign in passwordSighns)
+                stringToReturn.Append(sign);
+            return stringToReturn.ToString();
+        }
     }
 }
